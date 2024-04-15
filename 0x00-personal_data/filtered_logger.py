f04180e029136f7b4cca573ad14e729ff55eeb5a
Engr Ugwu <fugwungene@gmail.com>
2024-04-15 00:00:17 +0000

    Modify code.

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to obfuscate log messages.
+"""
+import logging
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Returns:
+        str: The obfuscated log message.
+    """
+    regex_pattern = '|'.join([f'{field}=([^.]+)([;$]+)' for field in fields])
+    return re.sub(regex_pattern, redaction, message)
