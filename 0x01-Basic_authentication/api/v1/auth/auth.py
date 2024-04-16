ad0fa50344f22c0a44afc1a36893cce2d5b30346
Engr Ugwu <fugwungene@gmail.com>
2024-04-16 10:47:39 +0000

    Implement task 3.
    Add the auth folder.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Authentication class"""
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Return None"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Return None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Return None"""
+        return None
