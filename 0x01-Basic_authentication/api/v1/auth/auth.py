25869229d99049d5bc9d8ca789b7e2a4fa06e0d7
Engr Ugwu <fugwungene@gmail.com>
2024-04-16 11:20:56 +0000

    Add class documentation.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return None"""
         return False
