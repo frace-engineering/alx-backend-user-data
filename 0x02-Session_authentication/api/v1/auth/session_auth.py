aae4c0676b5f492cabd90133bb7d1778d8b90ced
Engr Ugwu <fugwungene@gmail.com>
2024-04-18 14:25:14 +0000

    Fix pycode style issue in code.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,13 +5,15 @@ from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """Genarate the session id."""
+    """Session Authentication class"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
+        """Genarage session id"""
         if user_id is None or not isinstance(user_id, str):
             return None
         else:
             session_id = str(uuid.uuid4())
             if user_id not in self.user_id_by_session_id:
-                self.user_id_by_session_id[user_id] = session_id 
+                self.user_id_by_session_id[user_id] = session_id
             return session_id
