059c06a64c7e8fb54f7c6e83d5bb0fff42f9d2e0
Engr Ugwu <fugwungene@gmail.com>
2024-04-18 14:09:17 +0000

    Implement task 2.
    Add session id.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Creat session class"""
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """Do nothing for now."""
-    pass
+    """Genarate the session id."""
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            if user_id not in self.user_id_by_session_id:
+                self.user_id_by_session_id[user_id] = session_id 
+            return session_id
