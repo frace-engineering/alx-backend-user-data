bd050e2e11bf6ee058d39a0fdbdd8c37ab4e190c
Engr Ugwu <fugwungene@gmail.com>
2024-04-24 17:23:43 +0000

    Fix pycodestyle issue.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Hashed password generation"""
 import bcrypt
+from db import DB, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,7 +12,7 @@ def _hash_password(password: str) -> bytes:
         password.
     Return:
         hashed password
-        
+
     We encode password because bcrypt's hashpw accepts without error
     bytes string but raises error with unicode string
     """
@@ -20,18 +22,15 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
 
-from db import DB, User
-
 
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        from sqlalchemy.orm.exc import NoResultFound
+        """Register a user"""
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exist')
@@ -40,6 +39,3 @@ class Auth:
         hashed_password = self._hash_passpword(password)
         self._db.add_user(email=email, hashed_password=hashed_password)
         return user
-
-
-
